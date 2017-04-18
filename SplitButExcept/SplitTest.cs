9cb351270978f6b1e2e7496a0786bad3b300015a
Kuei-PC\Kuei <ragnakuei@livemail.tw>
2017-04-18 09:21:30 +0800

    4th red light

--- SplitButExcept/SplitTest.cs
+++ SplitButExcept/SplitTest.cs
@@ -7,7 +7,7 @@ namespace SplitButExcept
     public class SplitTest
     {
         [TestMethod]
-        public void Result_Test_0()
+        public void Result_Test_1()
         {
             var input = "ab,bc";
             var actual = new Split(',', '"', input).Result();
@@ -16,11 +16,20 @@ namespace SplitButExcept
         }
 
         [TestMethod]
-        public void Result_Test_1()
+        public void Result_Test_2()
         {
             var input = "ab,\"bc,cd\"";
             var actual = new Split(',', '"', input).Result();
             string[] expected = { "ab", "bc,cd" };
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+
+        [TestMethod]
+        public void Result_Test_3()
+        {
+            var input = "ab,\"bc,cd\",\"de\"";
+            var actual = new Split(',', '"', input).Result();
+            string[] expected = { "ab", "bc,cd","de" };
             expected.ToExpectedObject().ShouldEqual(actual);
         }
     }
